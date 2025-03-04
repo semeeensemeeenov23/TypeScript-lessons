afd8f35743e7d02caa6200ba7f7c6d51ade59351
ilmelnikof <ilmelnikof@gmail.com>
2025-03-04 13:39:30 +0500

    lesson-21

--- app.ts
+++ app.ts
@@ -43,3 +43,30 @@ logId(1);
 logId('lklk');
 logId(true);
 
+const arr = ['sdf', 1];
+
+function logId2(id: string | number | boolean) {
+    if (typeof id === 'string') {
+        console.log(id.toLowerCase)
+    } else if (typeof id === 'number') {
+        console.log(id)
+    } else {
+        console.log(id)
+    }
+}
+
+function logError(err: string | string[]) {
+    if (Array.isArray(err)) {
+        console.log(err)
+    } else {
+        console.log(err)
+    }
+}
+
+function logObject(obj: {a : number} | {b : number}) {
+    if ('a' in obj) {
+        console.log(obj.a)
+    } else {
+        console.log(obj.b)
+    }
+}
